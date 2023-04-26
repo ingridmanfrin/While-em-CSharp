151397e22513c339e45e92db4c31ffddf0337ac1
ingridmanfrin <ingrid.caldeira@ITLNB109>
2023-04-26 02:59:47 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace While_em_CSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ListBox = new System.Windows.Forms.ListBox();
+            this.BtnOK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ListBox
+            // 
+            this.ListBox.FormattingEnabled = true;
+            this.ListBox.Location = new System.Drawing.Point(96, 39);
+            this.ListBox.Name = "ListBox";
+            this.ListBox.Size = new System.Drawing.Size(248, 251);
+            this.ListBox.TabIndex = 0;
+            // 
+            // BtnOK
+            // 
+            this.BtnOK.Location = new System.Drawing.Point(185, 310);
+            this.BtnOK.Name = "BtnOK";
+            this.BtnOK.Size = new System.Drawing.Size(75, 23);
+            this.BtnOK.TabIndex = 1;
+            this.BtnOK.Text = "OK";
+            this.BtnOK.UseVisualStyleBackColor = true;
+            this.BtnOK.Click += new System.EventHandler(this.BtnOK_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(486, 384);
+            this.Controls.Add(this.BtnOK);
+            this.Controls.Add(this.ListBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox ListBox;
+        private System.Windows.Forms.Button BtnOK;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace While_em_CSharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnOK_Click(object sender, EventArgs e)
+        {
+            //WHILE;
+            int vezes = 0;
+            while (vezes <= 100)
+            {
+                ListBox.Items.Add(vezes);
+                vezes++;
+            }
+            // O exemplo de cima e este de baixo faz exatamente o mesmo efeito:
+            //int vezes = 0;
+            //while (vezes <= 100)
+            //{
+            //    ListBox.Items.Add(vezes++);
+            //}
+
+            //DO WHILE;
+            //    int vezes = 0;
+            //    do
+            //    {
+            //        ListBox.Items.Add(vezes++);
+
+            //    } while (vezes <= 100);
+            //}
+        }
+    }
+}
